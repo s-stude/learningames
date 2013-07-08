09a28359b7797bbfc0435b19042883a7e738da66
Akim Khalilov <Akim.Khalilov@gmail.com>
2013-07-08 19:19:47 +0300

    Tweaking gruntfile to watch & copy categories of files (appjs, vendorjs, appcss, apptemplates, etc.)

--- src/games/puzzles/Gruntfile.js
+++ src/games/puzzles/Gruntfile.js
@@ -20,8 +20,7 @@ module.exports = function (grunt) {
                 }
             },
 
-
-            gruntfile: ['gruntfile.js'],
+            gruntfile: ['Gruntfile.js'],
 
             appjs: ['app/scripts/main.js', 'app/scripts/**/*.js', '!app/scripts/vendor/**/*.js']
         },
@@ -31,7 +30,6 @@ module.exports = function (grunt) {
                 options: {
                     baseUrl       : "app/scripts",
                     mainConfigFile: "app/scripts/main.js",
-                    //out           : "<%= vars.dist %>/scripts/<%= vars.distjs %>",
                     name          : "main",
                     dir           : '<%= vars.dist %>/scripts/',
                     optimize      : "uglify2",
@@ -44,29 +42,92 @@ module.exports = function (grunt) {
             },
             release: {
                 options: {
-                    baseUrl       : "app/scripts",
-                    mainConfigFile: "app/scripts/main.js",
-                    //out           : "<%= vars.dist %>/scripts/<%= vars.distjs %>",
-                    name          : "main",
-
+                    baseUrl                : "app/scripts",
+                    mainConfigFile         : "app/scripts/main.js",
+                    name                   : "main",
                     dir                    : '<%= vars.dist %>/scripts/',
                     preserveLicenseComments: false,
                     optimize               : "uglify2",
                     generateSourceMaps     : true
-//                    modules: [
-//                        {
-//                            name   : "main",
-//                            include: [ "text" ]
-//                        }
-//                    ]
                 }
 
             }
         },
 
         watch: {
-            files: ['<%= jshint.gruntfile %>', '<%= jshint.appjs %>', 'app/*.html', 'app/css/**/*.css'],
-            tasks: ['default']
+            appjs       : {
+                files: ['app/scripts/main.js', 'app/scripts/**/*.js', '!app/scripts/vendor/**/*.js'],
+                tasks: ['jshint', 'copy:appjs']
+            },
+            vendorjs    : {
+                files: ['app/scripts/vendor/**/*.js'],
+                tasks: ['copy:vendorjs']
+            },
+            appcss      : {
+                files: ['app/css/**/*.css'],
+                tasks: ['copy:appcss']
+            },
+            apptemplates: {
+                files: ['app/index.html', 'app/scripts/**/*.html'],
+                tasks: ['copy:apptemplates']
+            }
+        },
+
+        copy: {
+            appjs: {
+                files: [
+                    {
+                        expand: true,
+                        cwd   : 'app/',
+                        src   : ['scripts/main.js', 'scripts/**', '!scripts/vendor/**'],
+                        dest  : '<%= vars.dist %>/'
+                    }
+                ]
+            },
+
+            vendorjs: {
+                files: [
+                    {
+                        expand: true,
+                        cwd   : 'app/',
+                        src   : ['scripts/vendor/**'],
+                        dest  : '<%= vars.dist %>/'
+                    }
+                ]
+            },
+
+            appcss: {
+                files: [
+                    {
+                        expand: true,
+                        cwd   : 'app/',
+                        src   : ['css/**'],
+                        dest  : '<%= vars.dist %>/'
+                    }
+                ]
+            },
+
+            apptemplates: {
+                files: [
+                    {
+                        expand: true,
+                        cwd   : 'app/',
+                        src   : ['index.html', 'scripts/**/*.html'],
+                        dest  : '<%= vars.dist %>/'
+                    }
+                ]
+            },
+
+            release: {
+                files: [
+                    {
+                        expand: true,
+                        cwd   : 'app/',
+                        src   : ['index.html', 'scripts/vendor/requirejs/require.js'],
+                        dest  : '<%= vars.dist %>/'
+                    }
+                ]
+            }
         },
 
         connect: {
@@ -85,36 +146,8 @@ module.exports = function (grunt) {
             }
         },
 
-        copy: {
-            dev    : {
-                files: [
-                    {
-                        expand: true,
-                        cwd   : 'app/',
-                        src   : ['index.html', 'scripts/**', 'css/**', 'fonts/**', 'images/**'],
-                        dest  : '<%= vars.dist %>/'
-                    },
-                    {
-                        expand : true,
-                        flatten: true,
-                        cwd    : 'app/',
-                        src    : 'scripts/vendor/requirejs-text/text.js', dest: '<%= vars.dist %>/scripts'
-                    }
-                ]
-            },
-            release: {
-                files: [
-                    {
-                        expand: true,
-                        cwd   : 'app/',
-                        src   : ['index.html', 'scripts/vendor/requirejs/require.js'],
-                        dest  : '<%= vars.dist %>/'
-                    }
-                ]
-            }
-        },
 
-        clean: ['<%= vars.dist %>']
+        clean: ['<%= vars.dist %>/']
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
@@ -128,7 +161,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('default', ['clean', 'jshint', 'copy:dev']);
+    grunt.registerTask('default', ['clean', 'jshint', 'copy:appjs', 'copy:vendorjs', 'copy:appcss', 'copy:apptemplates']);
     grunt.registerTask('live-dev', ['default', 'connect:dev', 'watch']);
 
     grunt.registerTask('release', ['clean', 'jshint', 'copy:release', 'requirejs']);
