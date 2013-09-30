0170f078ecbf05e57f45b655147694660acaedba
gcisevs <germancishevskiy@gmail.com>
2013-09-30 21:37:35 +0300

    Adding polygon.js

---
+++ app/widgets/sum/polygon.js
@@ -0,0 +1,42 @@
+define(function(require) {
+    var _ = require('Underscore');
+
+    return (function() {
+
+        var create = function(props) {
+
+            var polygon = props.paper.path('M' + props.startX + ',' + props.startY + ",l-60,-30l0,-60l60,-30l60,30l0,60z");
+
+            polygon.attr({
+                stroke: '#1abc9c',
+                'stroke-opacity': 0.4,
+                'stroke-width': 4,
+                fill: 'white',
+                cursor: 'pointer'
+
+            });
+
+            polygon.data('index', props.index);
+
+
+            return polygon;
+        },
+            createText = function(props) {
+                var text = props.paper.text(props.startX, props.startY, _.random(2, 9));
+                text.attr({
+                    'font-family': 'Lato, sans-serif',
+                    'font-size': '25',
+                    fill: '#e74c3c',
+                    cursor: 'pointer'
+                });
+                text.data('index', props.index);
+                return text;
+            };
+
+        return {
+            create: create,
+            createText: createText
+        };
+    })();
+
+});
