38017c19369306c01ba0eef9ab163a3ab7fc3c62
gcishevs <germancishevskiy@gmail.com>
2013-10-17 18:25:08 +0300

    changed random return for the polygon

--- app/widgets/sum/polygon.js
+++ app/widgets/sum/polygon.js
@@ -22,7 +22,7 @@ define(function(require) {
             return polygon;
         },
             createText = function(props) {
-                var text = props.paper.text(props.startX, props.startY, _.random(1, 1));
+                var text = props.paper.text(props.startX, props.startY, _.random(2, 9));
                 text.attr({
                     'font-family': 'Lato, sans-serif',
                     'font-size': '25',
